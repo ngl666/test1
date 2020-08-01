2a76a456307d24d72a5eaf0165494a2eb809a688
niu guoliang <2438282114@qq.com>
2020-08-01 18:43:35 +0800

    第一次尝试提交到本地仓库

--- src/cn/anyglobe/hello.java
+++ src/cn/anyglobe/hello.java
@@ -3,5 +3,6 @@ package cn.anyglobe;
 public class hello {
     public static void main(String[] args){
         System.out.println("Hello java,my name is 牛国亮");
+        System.out.println("Hello java,my name is 牛国亮");
     }
 }
